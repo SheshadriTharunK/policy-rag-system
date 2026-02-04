d68f231599704d00f41b67d29b453ccaa2f714c2
Tharun Sheshadri <97800608+SheshadriTharunK@users.noreply.github.com>
2026-02-04 17:45:49 +0530

    Change vector_db_dir to relative path
    Updated vector_db_dir path to a relative format.

--- app/rag.py
+++ app/rag.py
@@ -28,9 +28,7 @@ def build_context(vector_db: FAISS, query: str, top_k: int):
     return query + "\n<context>\n" + res + "\n</context>"
 
 # loading the vector DB
-vector_db_dir = os.path.expanduser(
-    "~\\data\\semantic-search\\index\\faiss\\faiss1"
-)
+vector_db_dir = "data\semantic-search\index\faiss\faiss1"
 
 # initialize embeddings model
 embeddings_model = HuggingFaceEmbeddings(
