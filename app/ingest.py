cbf9993c6058bb4b2c37d9ffa19da08252994dd4
TharunSheshadriK <sheshadritharun@gmail.com>
2026-02-05 13:48:48 +0530

    commit for devbranch for policies

--- app/ingest.py
+++ app/ingest.py
@@ -7,14 +7,16 @@ from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores.faiss import DistanceStrategy
 from langchain_community.document_loaders import TextLoader
-from sentence_transformers import SentenceTransformer
 
 load_dotenv(override=True)
 time.sleep(1)
 
-# load documents from local directory
-kb_dir = os.path.expanduser("~/Desktop/Advintek/data")
-print(f"Loading data from {kb_dir}")
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / "data"
+
+print(f"Base dir: {BASE_DIR}")
+print(f"Loading documents from: {DATA_DIR}")
+
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,
     chunk_overlap=80,
@@ -23,12 +25,14 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 
 documents = []
-for file in Path(kb_dir).glob("*.txt"):
+for file in Path(DATA_DIR).glob("*.txt"):
     loader = TextLoader(file)
     documents.extend(loader.load_and_split(text_splitter))
 print(len(documents))
-print(documents)
-
+print("these are the documents",documents)
+for i, doc in enumerate(documents[:2]):
+    print(f"\n--- Chunk {i} ---")
+    print(doc.page_content[:300])
 
 # create embeddings for all the chunks and store them in vector database
 embeddings_model = HuggingFaceEmbeddings(
@@ -41,8 +45,9 @@ embeddings_model = HuggingFaceEmbeddings(
 vector_db = FAISS.from_documents(
     documents=documents,
     embedding=embeddings_model,
-    distance_strategy=DistanceStrategy.EUCLIDEAN_DISTANCE,
+    distance_strategy=DistanceStrategy.COSINE,
 )
-vector_db_dir = os.path.expanduser("~/Desktop/Advintek/data/semantic-search/index/faiss")
+VECTOR_DB_DIR = BASE_DIR / "data" / "semantic-search" / "index" / "faiss"
+VECTOR_DB_DIR.mkdir(parents=True, exist_ok=True)
 # save vector DB to local directory
-vector_db.save_local(folder_path=vector_db_dir)
+vector_db.save_local(folder_path=VECTOR_DB_DIR)
--- app/run_app.py
+++ app/run_app.py
@@ -1,6 +1,137 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from pydantic_ai import Agent
+import os
+import time
+from pathlib import Path
+from dotenv import load_dotenv
+from pydantic_ai.models.groq import GroqModel
+from langchain_community.vectorstores import FAISS
+from pydantic_ai.models.groq import GroqModel
+from pydantic_ai.providers.groq import GroqProvider
 from fastapi import FastAPI
-from pydantic import BaseModel
-from rag import answer_question
+from app.rag import answer_question
+
+load_dotenv(override=True)
+time.sleep(1)
+system_prompt = """
+You are a company policy assistant.
+
+Your task is to answer employee questions STRICTLY using the provided policy context.
+Do NOT use outside knowledge.
+Do NOT make assumptions.
+
+If the answer is not explicitly present in the context, respond exactly with:
+"I could not find this information in the company policies."
+
+--------------------
+Examples:
+
+Context:
+"Employees are entitled to 10 days of sick leave per year."
+
+Question:
+"How many sick leave days are employees allowed?"
+
+Answer:
+"Employees are entitled to 10 days of sick leave per year."
+
+---
+
+Context:
+"Employees must obtain prior approval from their reporting manager before opting for WFH."
+
+Question:
+"Do employees need approval to work from home?"
+
+Answer:
+"Yes, employees must obtain prior approval from their reporting manager before opting for work from home."
+
+---
+
+Context:
+"The following holidays are observed across all company locations:
+Republic Day – January 26
+Independence Day – August 15"
+
+Question:
+"Is Republic Day a company holiday?"
+
+Answer:
+"Yes, Republic Day on January 26 is observed as a company holiday across all company locations."
+---
+
+Context:
+"There is no information in the provided policies about employee stock options."
+
+Question:
+"Do employees receive stock options?"
+
+Answer:
+"I could not find this information in the company policies."
+
+--------------------
+Only answer using the given context.
+"""
+
+
+# defining an agent with groq model
+model  = GroqModel(
+         model_name = "llama-3.1-8b-instant",
+            provider = GroqProvider(api_key=os.getenv("GROQ_API_KEY")))
+rag_agent = Agent(model,
+                  system_prompt=system_prompt)
+
+# function to build context from vector DB
+def build_context(vector_db: FAISS, query: str, top_k: int):
+    print("Building context for query:", vector_db)
+    base_retriever = vector_db.as_retriever(search_kwargs={"k": top_k})
+    hits = base_retriever.invoke(query)
+    print("Hits retrieved:", hits)
+    res = "\n".join([hit.page_content for hit in hits])
+    return f"""
+        Context:
+        {res}
+
+        Question:
+        {query}
+        """
+
+# loading the vector DB
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+vector_db_dir = BASE_DIR / "data" / "semantic-search"/"index"/"faiss"
+print(f"Loading vector DB from {vector_db_dir}")
+# initialize embeddings model
+embeddings_model = HuggingFaceEmbeddings(
+    model_name = os.getenv("HF_EMBEDDINGS_MODEL"),
+    encode_kwargs = {"normalize_embeddings": True},
+    model_kwargs = {"token": os.getenv("HUGGING_FACE_TOKEN")},
+)
+# load vector DB from local directory
+import os
+print(os.getcwd())
+vector_db = FAISS.load_local(
+    folder_path = vector_db_dir,
+    embeddings = embeddings_model,
+    allow_dangerous_deserialization = True,
+)
+docs = vector_db.similarity_search("holiday", k=5)
+for i, d in enumerate(docs):
+    print(f"\n--- Doc {i} ---")
+    print(d.page_content)
+
+
+# function to answer question using RAG approach
+async def answer_question(question: str) -> str:
+    query_with_context = build_context(
+        vector_db = vector_db,
+        query = question,
+        top_k = 8,
+    )
+    print("Query with context:", query_with_context)
+    agent = rag_agent
+    result = await agent.run(query_with_context)
+    return result.output 
 
 application = FastAPI(title="Policy System Advintek")
 
@@ -12,3 +143,4 @@ async def ask_question(question: str):
     result = await answer_question(question)  
     lines = result.replace("\n", "")
     return {"answer": lines}
+
